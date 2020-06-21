340950efb07482b09c8f901d4f34c14c9f5dcc4e
shugoY45 <s.yoshida.aogiri45@gmail.com>
2020-06-21 12:00:29 +0900

    #21 クラスメソッド、クラス変数を使おう

--- hello.rb
+++ hello.rb
@@ -1,12 +1,20 @@
 # クラス
+# クラスメソッド
+# クラス変数
+# 定数
 
 class User
+
+  @@count = 0
+
+  VERSION = 1.1
 
   attr_accessor :name
   # setter: name=(value)
   # getter: name
 
   def initialize(name)
+    @@count += 1
     @name = name
   end
 
@@ -14,20 +22,33 @@ class User
     # self
     # self.name -> @name
     puts "hi! i am #{@name}"
-    puts "hi! i am #{self.name}"
-    puts "hi! i am #{name}"
-
-
+    # puts "hi! i am #{self.name}"
+    # puts "hi! i am #{name}"
   end
+
+  def self.info
+    puts "#{VERSION}: User Class, #{@@count} instance."
+  end
+
 
 end
 
 tom = User.new("tom")
+bob = User.new("bob")
+steve = User.new("steve")
 
-tom.name = "tom Jr."
-p tom.name
+User.info
+p User::VERSION
 
-tom.sayHi #レシーバー
+# din = User.new("din")
+# p din.info
+
+  # tom = User.new("tom")
+#
+# tom.name = "tom Jr."
+# p tom.name
+#
+ # tom.sayHi #レシーバー
 #
 #
 # bob = User.new("bob")
