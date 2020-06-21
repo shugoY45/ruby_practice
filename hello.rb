facbdb40cc60ec478e8dc02564afcc55439f40cd
shugoY45 <s.yoshida.aogiri45@gmail.com>
2020-06-21 18:02:04 +0900

    #20 アクセサを使ってみよう

--- hello.rb
+++ hello.rb
@@ -2,19 +2,33 @@
 
 class User
 
+  attr_accessor :name
+  # setter: name=(value)
+  # getter: name
+
   def initialize(name)
     @name = name
   end
 
   def sayHi
+    # self
+    # self.name -> @name
     puts "hi! i am #{@name}"
+    puts "hi! i am #{self.name}"
+    puts "hi! i am #{name}"
+
+
   end
 
 end
 
 tom = User.new("tom")
-tom.sayHi
 
+tom.name = "tom Jr."
+p tom.name
 
-bob = User.new("bob")
-bob.sayHi
+tom.sayHi #レシーバー
+#
+#
+# bob = User.new("bob")
+# bob.sayHi
