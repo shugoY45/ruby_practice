d3bd9b3e6b4797ae66c59e7682f066bf03ddbda4
shugoY45 <s.yoshida.aogiri45@gmail.com>
2020-06-21 22:07:46 +0900

    #23 メソッドのアクセス権を理解しよう

--- hello.rb
+++ hello.rb
@@ -1,71 +1,37 @@
-# クラスの継承
+# アクセス権
+# -public
+# -protected
+# -private :レシーバーを指定できない
+
 
 class User
 
-  @@count = 0
-
-  VERSION = 1.1
-
-  attr_accessor :name
-  # setter: name=(value)
-  # getter: name
-
-  def initialize(name)
-    @@count += 1
-    @name = name
-  end
-
   def sayHi
-    # self
-    # self.name -> @name
-    puts "hi! i am #{@name}"
-    # puts "hi! i am #{self.name}"
-    # puts "hi! i am #{name}"
+    puts "hi!"
+    sayPrivate
+    #self.sayPrivate
   end
 
-  def self.info
-    puts "#{VERSION}: User Class, #{@@count} instance."
-  end
+  private
+    def sayPrivate
+      puts "private"
+    end
+
 end
-
-# User: 親クラス、super class
-# AdminiUser: 子クラス、 Sub Class
 
 class AdminUser < User
-
-
-  def sayHello
-    puts "Hello from #{@name}"
+  # def sayHello
+  #   puts "Hello"
+  #   sayPrivate
+  # end
+  def sayPrivate
+    puts "private from Admin"
   end
 
-# オーバーライド
-  def sayHi
-    puts "hi! from admin!"
-  end
 end
 
+# User.new.sayPrivate #NG
+#User.new.sayHi
 
-tom = AdminUser.new("tom")
-tom.sayHi
-tom.sayHello
-
-# tom = User.new("tom")
-# bob = User.new("bob")
-# steve = User.new("steve")
-#
-# User.info
-# p User::VERSION
-
-# din = User.new("din")
-# p din.info
-
-  # tom = User.new("tom")
-#
-# tom.name = "tom Jr."
-# p tom.name
-#
- # tom.sayHi #レシーバー
-#
-#
-# bob = User.new("bob")
-# bob.sayHi
+#AdminUser.new.sayHello
+AdminUser.new.sayPrivate
