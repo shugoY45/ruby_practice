2457594497cc0e4b3d56779ddaad12e6793634ce
shugoY45 <s.yoshida.aogiri45@gmail.com>
2020-06-21 23:02:09 +0900

    #26 例外を扱ってみよう

--- hello.rb
+++ hello.rb
@@ -1,20 +1,20 @@
-# module
-# -ミックスイン
+# 例外
 
-module Debug
+class MyError < StandardError ; end
 
-  def info
-    puts "#{self.class} debug info ..."
+x = gets.to_i
+
+begin
+  if x == 3
+    raise MyError
   end
+  p 100 / x
+rescue MyError
+  puts "not 3!"
+rescue => ex
+  p ex.message
+  p ex.class
+  puts "stopped!"
+ensure
+  puts "-- END --"
 end
-
-class Player
-  include Debug
-end
-
-class Monster
-  include Debug
-end
-
-Player.new.info
-Monster.new.info
